502e336e5044078573c24074039059b12fc8e582
shehuna <shehun.ayele@gmail.com>
2023-08-26 00:27:07 +0300

    Mega Menu

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import News from "./pages/News";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <>
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/services" element={<Services/>}/>
         <Route path="/news" element={<News/>}/>
       </Routes>
+      <Footer/>
     </Router>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import logo from '../assets/logo/emi.png'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+export default function Header() {
+  const location = useLocation()
+  const navigate = useNavigate()
+  function pathMatchRoute(route){
+    if(route === location.pathname){
+        return true
+    }
+  }
+  return (
+    <div className='bg-white border-b shadow-sm sticky 
+    top-0 z-50'>
+        <header className='flex justify-between items-center
+        px-3 max-w-6xl mx-auto opacity-90'>
+            <div>
+                <img src={logo} alt="" className='cursor-pointer' onClick={()=>navigate("/")}/>
+            </div>
+            <div>
+                <ul className='flex space-x-10 uppercase'>
+                    <li  onClick={()=>navigate("/")} className={` cursor-pointer py-9 text-sm font-semibold 
+                    text-gray-400 border-b-[3px] border-b-transparent hover:border-b-blue-500
+                     ${pathMatchRoute("/") && "text-black border-b-blue-500"}`}>Home
+                     </li>
+                    <li  onClick={()=>navigate("/about")} className={`relative group cursor-pointer py-9 text-sm font-semibold 
+                    text-gray-400 border-b-[3px] border-b-transparent 
+                     ${pathMatchRoute("/about") && "text-black border-b-blue-500"}`}>About
+                     <div className='absolute top-0 -left-96 transition
+                     group-hover:translate-y-5 translate-y-0 opacity-0
+                     invisible group-hover:opacity-20 group-hover:visible
+                     duration-500 ease-in-out group-hover:transition
+                     z-50 min-w-[700px] transform'>
+                     <div className='relative top-[75px] p-6 bg-black
+                     shadow-xl w-full'>
+                       <div className='w-10 h-10 bg-black
+                       transform rotate-45 absolute top-0 z-0
+                       translate-x-0 transition-transform 
+                       group-hover:translate-x-[23rem] duration-500
+                       ease-in-out rounded-sm'></div>
+                     </div>
+                     </div></li>
+                    <li  onClick={()=>navigate("/services")} className={`cursor-pointer py-9 text-sm font-semibold 
+                    text-gray-400 border-b-[3px] border-b-transparent hover:border-b-blue-500
+                     ${pathMatchRoute("/services") && "text-black border-b-blue-500"}`}>Service</li>
+                    <li  onClick={()=>navigate("/news")} className={`cursor-pointer py-9 text-sm font-semibold 
+                    text-gray-400 border-b-[3px] border-b-transparent hover:border-b-blue-500
+                     ${pathMatchRoute("/news") && "text-black border-b-blue-500"}`}>News</li>
+                    <li  onClick={()=>navigate("/contact")} className={`cursor-pointer py-9 text-sm font-semibold 
+                    text-gray-400 border-b-[3px] border-b-transparent hover:border-b-blue-500
+                     ${pathMatchRoute("/contact") && "text-black border-b-blue-500"}`}>Contact</li>
+                </ul>
+            </div>
+        </header>
+    </div>
+  )
+}
